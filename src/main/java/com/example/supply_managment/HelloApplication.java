df6a8cd965dd5015d7d2e8955c4a66988339460b
Your Name <you@example.com>
2022-12-17 16:09:01 +0530

    Login Page

--- src/main/java/com/example/supply_managment/HelloApplication.java
+++ src/main/java/com/example/supply_managment/HelloApplication.java
@@ -3,15 +3,39 @@ package com.example.supply_managment;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private GridPane loginPage(){
+        Label email = new Label("Email");
+        Label password = new Label("Password");
+        TextField emailField = new TextField("Enter Your email");
+        PasswordField passwordField = new PasswordField();
+        GridPane gridPane = new GridPane();
+
+        gridPane.add(email,0,0);
+        gridPane.add(emailField,1,0);
+        gridPane.add(password,0,1);
+        gridPane.add(passwordField,1,1);
+        return gridPane;
+    }
+    private Pane creatContent(){
+        Pane root = new Pane();
+        root.getChildren().addAll(loginPage());
+        return root;
+    }
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(creatContent());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
